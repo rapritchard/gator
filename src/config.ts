b05710f1287e8800a90999d9e25e5f7f69fbece9
Reece Pritchard <reece.pritchard@proton.me>
2026-09-11 14:56:04 +0100

    feat: add config

---
+++ src/config.ts
@@ -0,0 +1,57 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+export type Config = {
+  dbUrl: string;
+  currentUserName?: string;
+}
+
+function getConfigFilePath(): string {
+  return path.join(os.homedir(), ".gatorconfig.json");
+}
+
+function validateConfig(rawConfig: any): Config {  
+  if (!rawConfig.db_url || typeof rawConfig.db_url !== "string") {
+    throw new Error("Config error: A 'db_url' is required.")
+  }
+
+  const cfg: Config = {
+    dbUrl: rawConfig.db_url,
+    currentUserName: rawConfig.current_user_name ?? ""
+  };
+
+  return cfg;
+}
+
+export function setUser(user: string) {
+  const cfg = readConfig();
+  cfg.currentUserName = user;
+  writeConfig(cfg);
+}
+
+function writeConfig(cfg: Config) {
+  const configFilePath = getConfigFilePath();
+  const configToSave = {
+    db_url: cfg.dbUrl,
+    current_user_name: cfg.currentUserName
+  };
+  fs.writeFileSync(configFilePath, JSON.stringify(configToSave));
+}
+
+export function readConfig(): Config {
+  const configFilePath = getConfigFilePath();
+  const rawConfig = fs.readFileSync(configFilePath, {
+    encoding: "utf-8"
+  });
+  
+  let parsedConfig;
+    
+  try {
+    parsedConfig = JSON.parse(rawConfig);
+    return validateConfig(parsedConfig);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { readConfig, setUser } from "./config.js";
+
 function main() {
-  console.log("Hello, world!");
+  setUser("Reece");
+  const cfg = readConfig();
+  console.log(cfg);
 }
 
 main();
